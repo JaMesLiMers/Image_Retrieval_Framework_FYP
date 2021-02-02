c4cb64a414095e2949d3a9f163a9f8bfe0f90430
jameslimer <jameslimers@gmail.com>
2021-02-02 18:44:32 +0800

    add comment,add weight logic,add ip and port

--- TestModel/testServer.py
+++ TestModel/testServer.py
@@ -13,19 +13,72 @@ sys.path.append(project_path)
 from Models.Bm25LMIR.archLmirBm25Model import archLmirBm25Model
 
 class PostHandler(BaseHTTPRequestHandler):
-    # how to use:
+    """
+    Http服务的handler类, 实现了post请求的处理.
+
+        要求post请求的'Content-type'为'application/json'而且具有我
+        们下面定义的输入内容, 否则返回400.
+            可识别的输入内容示例:
+                {
+                    "query": ["图书馆", "苏州"],
+                    "weight": [0.1, 0.9]
+                }
+            返回的json文件格式如下:
+                {
+                    "result":{
+                        "imageId":[图片id列表],
+                        "imagePath":[图片path列表],
+                        "imageTitle":[图片title列表],
+                        "imageAnno": [要显示的图片标注列表],
+                        }
+                }
+
+        TODO: 
+        1. 添加注释内容. [Done]
+        2. 增加有weight和无weight的不同行为, 目前默认使用weight. [Done]
+        3. 增加服务器启动的参数, 包括ip地址和port. [Done]
+        4. 测试可用性. [Done]
+    """
+    # 在实例化之前加载的全局变量(查询类)
     archPath = "./Dataset/Arch/DemoData_20201228.json"
-    # init model
     model = archLmirBm25Model(archPath=archPath)
 
     def _information_retrieval(self, query, weight):
+        """
+        使用weight(或者不用)和query进行信息检索的方法.
+
+        Input:
+            query: 处理好的字符串列表.
+            weight: 处理好的权重列表, 可以留空.
+        
+        Return:
+            返回格式如下的python字典:
+                {
+                    "result":{
+                        "imageId":[图片id列表],
+                        "imagePath":[图片path列表],
+                        "imageTitle":[图片title列表],
+                        "imageAnno": [要显示的图片标注列表],
+                        }
+                }
+            若结果出了问题则返回空字典
+        """
+        # check weight is same length as query
+        useWeight = weight and len(weight) == len(query)
+
         # search a list of word/sentence
-        index, copora, annoIds , imageIds = self.model.searchSentence(listWords=query)
+        try:
+            if useWeight:
+                index, copora, annoIds , imageIds = self.model.searchWords(listWords=query, weights=weight)
+            else:
+                index, copora, annoIds , imageIds = self.model.searchSentence(listWords=query)
+        except Exception as e:
+            return {}
+
 
         # init result
         result = {}
         result["result"] = {}
-        result["result"]["imageId"] = imageIds
         imagePaths = []
         imageTitles = []
         imageAnno = []
@@ -46,21 +99,27 @@ class PostHandler(BaseHTTPRequestHandler):
 
             imageAnno.append(annoString)
         else:
+            result["result"]["imageId"] = imageIds
             result["result"]["imagePath"] = imagePaths
             result["result"]["imageTitle"] = imageTitles
             result["result"]["imageAnno"] = imageAnno
         return result
 
     def _set_headers(self):
+        """设定返回的头部"""
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
     def do_HEAD(self):
+        """返回头部"""
         self._set_headers()
 
     # POST echoes the message adding a JSON field
     def do_POST(self):
+        """
+        处理post请求, 保证输入为json格式并具有目标格式的内容, 并返回需要的结果(json格式)
+        """
         ctype, pdict = cgi.parse_header(self.headers.get('content-type'))
         
         # refuse to receive non-json content
@@ -73,9 +132,14 @@ class PostHandler(BaseHTTPRequestHandler):
         length = int(self.headers.get('content-length'))
         message = json.loads(self.rfile.read(length))
         
-        # add a property to the object, just to mess with data
-        query = message['query']
-        weight = message['weight']
+        # get input query and weight, if not exist, refuse.
+        try:
+            query = message['query']
+            weight = message['weight']
+        except KeyError as e:
+            self.send_response(400)
+            self.end_headers()
+            return
 
         # generate result
         result = self._information_retrieval(query, weight)
@@ -86,10 +150,18 @@ class PostHandler(BaseHTTPRequestHandler):
         return
 
 if __name__ == '__main__':
+    """启动server代码"""
+    import argparse
     from http.server import HTTPServer
-    server = HTTPServer(('localhost', 8080), PostHandler)
+    # add parser, now we can set ip and port.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", default="localhost", help="ip address of server", required=False)
+    parser.add_argument("-p", "--port", type=int, default=8080, help="port of server", required=False)
+    args = parser.parse_args()
+    # run server
+    server = HTTPServer((args.ip, args.port), PostHandler)
     print('Starting server, use <Ctrl-C> to stop')
     server.serve_forever()
 
     # test 
-    # curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"query":["a", "b"], "weight":[0.1, 0.9]}' http://127.0.0.1:8080
+    # curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"query":["a", "b"], "weight":[0.1, 0.9]}' -o testresult.json http://127.0.0.1:8080 
